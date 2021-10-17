c521b99905e3a93ad1e65b3e7754111d38aaf7fa
Szczudlo-Magdalena <lyzwinska.m@gmail.com>
2021-10-17 13:48:49 +0200

    lab1

---
+++ ViewModels/RandomBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp2.Models;
+
+namespace LibApp2.ViewModels
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
